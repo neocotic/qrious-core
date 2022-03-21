7930bb76596b9b005148a1cd4bf29ede8e6b56d1
LeoDog896 <LeoDog896@hotmail.com>
2022-03-21 13:40:46 +0000

    Use npm

--- test/text.test.ts
+++ test/text.test.ts
@@ -26,6 +26,14 @@ const exampleCom = `####### ### ##### #######
 #     #  # # ## ## # ### 
 ####### # ### ##  # #####`;
 
+// import fs from "fs"
+// import crypto from "crypto"
+// for (let i = 0; i < 4096; i++) {
+//   const randomStr = crypto.randomBytes(i).toString("hex")
+//   const code = renderText({ value:randomStr });
+//   fs.writeFileSync(`./test/resources/${randomStr}`, code)
+// }
+
 test('Ensure base example.com example is valid', () => {
   expect(renderText({ value: 'https://example.com' })).toBe(exampleCom);
 });
