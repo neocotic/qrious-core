87202eb9610d56eebd81d50d0996e606aad290c7
LeoDog896 <LeoDog896@hotmail.com>
2022-03-21 16:18:20 +0000

    Add resources

--- test/text.test.ts
+++ test/text.test.ts
@@ -1,4 +1,5 @@
-import { renderText, Frame } from '../src/index';
+import { renderText } from '../src/index';
+import { promises as fs } from "fs"
 
 const exampleCom = `####### ### ##### #######
 #     # #  ## #   #     #
@@ -26,28 +27,33 @@ const exampleCom = `####### ### ##### #######
 #     #  # # ## ## # ### 
 ####### # ### ##  # #####`;
 
-import fs from "fs"
-import crypto from "crypto"
-fs.writeFileSync("./test/resources.txt", "");
-for (let i = 0; i < 4096; i++) {
-  const randomStr = crypto.randomBytes(i).toString("hex")
+// import crypto from "crypto"
+// await fs.writeFile("./test/resources.txt", "");
+// for (let i = 1; i < 4096; i += 10) {
+//   const randomStr = crypto.randomBytes(i).toString("hex")
 
-  const randomStringBytes = new TextEncoder().encode(randomStr)
+//   const code = renderText({ value: randomStr });
 
-  const code = new Frame({ value: randomStr });
-
-  const codeBytes = new Uint8Array(code.buffer);
-
-  const combinedBuffer = new Uint8Array(randomStringBytes.length + codeBytes.length)
-  combinedBuffer.set(randomStringBytes, 0);
-  combinedBuffer.set(codeBytes, randomStringBytes.length)
-  
-  fs.appendFileSync(`./test/resources.txt`, combinedBuffer) // seperator is &&\n
-}
+//   await fs.appendFile(`./test/resources.txt`, `${randomStr}:${code}\t`) // seperator is &&\n
+// }
 
 test('Ensure base example.com example is valid', () => {
   expect(renderText({ value: 'https://example.com' })).toBe(exampleCom);
 });
+
+test('Ensure all resources are valid', async () => {
+  const resourceText = await fs.readFile("./test/resources.txt", "utf-8")
+
+  for (const entry of resourceText.split("\t")) {
+
+    if (!entry) continue;
+
+    const key = entry.split(":")[0]
+    const value = entry.split(":")[1]
+
+    expect(renderText({ value: key })).toBe(value)
+  }
+})
 
 test('Ensure options can be passed to text renderer', () => {
   expect(renderText({
