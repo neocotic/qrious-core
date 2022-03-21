38c40d0eb5c20e16619487553ed249719301a982
LeoDog896 <LeoDog896@hotmail.com>
2022-03-21 13:58:38 +0000

    Use buffers

--- test/text.test.ts
+++ test/text.test.ts
@@ -1,4 +1,4 @@
-import { renderText } from '../src/index';
+import { renderText, Frame } from '../src/index';
 
 const exampleCom = `####### ### ##### #######
 #     # #  ## #   #     #
@@ -26,13 +26,24 @@ const exampleCom = `####### ### ##### #######
 #     #  # # ## ## # ### 
 ####### # ### ##  # #####`;
 
-// import fs from "fs"
-// import crypto from "crypto"
-// for (let i = 0; i < 4096; i++) {
-//   const randomStr = crypto.randomBytes(i).toString("hex")
-//   const code = renderText({ value:randomStr });
-//   fs.writeFileSync(`./test/resources/${randomStr}`, code)
-// }
+import fs from "fs"
+import crypto from "crypto"
+fs.writeFileSync("./test/resources.txt", "");
+for (let i = 0; i < 4096; i++) {
+  const randomStr = crypto.randomBytes(i).toString("hex")
+
+  const randomStringBytes = new TextEncoder().encode(randomStr)
+
+  const code = new Frame({ value: randomStr });
+
+  const codeBytes = new Uint8Array(code.buffer);
+
+  const combinedBuffer = new Uint8Array(randomStringBytes.length + codeBytes.length)
+  combinedBuffer.set(randomStringBytes, 0);
+  combinedBuffer.set(codeBytes, randomStringBytes.length)
+  
+  fs.appendFileSync(`./test/resources.txt`, combinedBuffer) // seperator is &&\n
+}
 
 test('Ensure base example.com example is valid', () => {
   expect(renderText({ value: 'https://example.com' })).toBe(exampleCom);
