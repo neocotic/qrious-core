f301addac492828fb13de52b89d9810c6cbb6b79
LeoDog896 <LeoDog896@hotmail.com>
2022-03-25 12:22:50 +0000

    Flex and flex grow

--- demo/src/routes/index.svelte
+++ demo/src/routes/index.svelte
@@ -16,14 +16,16 @@
 
   let value = "https://example.com"
 </script>
-<select bind:value={selectedRenderSystem}>
-  {#each renderSystems as renderSystem}
-    <option value={renderSystem}>
-      {renderSystem.name}
-    </option>
-  {/each}
-</select>
-<input bind:value={value}>
+<div class="flex">
+  <select class="flex-shrink" bind:value={selectedRenderSystem}>
+    {#each renderSystems as renderSystem}
+      <option value={renderSystem}>
+        {renderSystem.name}
+      </option>
+    {/each}
+  </select>
+  <input class="flex-grow" bind:value={value}>
+</div>
 <h1 class="font-mono" style="line-height: {selectedRenderSystem.lineSpacing}">
   {@html selectedRenderSystem?.render(value)?.replaceAll("\n", "<br/>")?.replaceAll(" ", "&nbsp;")}
 </h1>
